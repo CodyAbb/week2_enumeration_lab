029170f9143e952d5b6b3b088269e5642e2341af
Cody Abbott <cody.abbott16@gmail.com>
2019-11-28 14:10:35 +0000

    added searchable planet function

--- start_point/star_system.rb
+++ start_point/star_system.rb
@@ -13,4 +13,14 @@ class StarSystem
     return planet_array.length
   end
 
+  def planet_names
+    planet_array = []
+    @planets.each{|planet| planet_array << planet.name}
+    return planet_array
+  end
+
+  def get_planet_by_name(search_planet)
+    return @planets.find{|planet| planet.name == search_planet}
+  end
+
 end
